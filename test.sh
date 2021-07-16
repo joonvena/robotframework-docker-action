b59cef428c703f6fc78b572b37f5df831ec63b42
Joonas Venäläinen <joonas.venalainen@anders.com>
2021-07-16 08:34:23 +0300

    Switch to composite action to allow more granual configuration of settings

---
+++ test.sh
@@ -0,0 +1,14 @@
+# Create reports folder
+REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
+TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
+sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
+
+docker run --shm-size=$ALLOWED_SHARED_MEMORY \
+  -e BROWSER=$BROWSER \
+  -e ROBOT_THREADS=$ROBOT_THREADS \
+  -e PABOT_OPTIONS=$PABOT_OPTIONS \
+  -e ROBOT_OPTIONS=$ROBOT_OPTIONS \
+  -v $REPORTS_DIR:/opt/robotframework/reports:Z \
+  -v $TESTS_DIR:/opt/robotframework/tests:Z \
+  --user $(id -u):$(id -g) \
+  ppodgorsek/robot-framework:latest
