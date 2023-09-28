425c63c6b08f38cf13e4ded8e24d0438ab695560
Felipe Holek Damasceno <felipeholek@gmail.com>
2023-09-28 09:51:08 -0300

    Add resources directory mount

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 # Create reports folder
 REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
+RESOURCES_DIR=$(pwd)/$ROBOT_RESOURCES_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
@@ -9,6 +10,7 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
+  -v $RESOURCES_DIR:/opt/robotframework/resources:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
   $ROBOT_RUNNER_IMAGE
